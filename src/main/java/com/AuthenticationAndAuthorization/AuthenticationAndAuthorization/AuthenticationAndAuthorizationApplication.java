eac4e451e0899165db726313cb1c29a55ed6c2da
jyothiprakash <jyothiprakash1108@gmail.com>
2025-11-29 15:11:29 +0530

    Initial commit - project skeleton

---
+++ src/main/java/com/AuthenticationAndAuthorization/AuthenticationAndAuthorization/AuthenticationAndAuthorizationApplication.java
@@ -0,0 +1,14 @@
+package com.AuthenticationAndAuthorization.AuthenticationAndAuthorization;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationAndAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationAndAuthorizationApplication.class, args);
+	}
+
+}
